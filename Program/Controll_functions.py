e55544d6827b4142f6829c05374b96ca41e4e6ce
Leandro Schwab Dias Carneiro <schwab_@hotmail.com>
2018-10-17 10:58:02 -0300

    funçoes banco de dados
    ADD Funçoes de resgatar e adicionar usuarios no banco de dados

---
+++ Program/Controll_functions.py
@@ -0,0 +1,10 @@
+def checkOnlineOne(ip,porta):
+    try:
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # qw12IPv4,tipo de socket
+        conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        conn.connect((ip, porta))  # Abre uma conexão com IP e porta especificados
+        conn.close()
+    except Exception as e:
+        print('Usuario Offline' + ip + ':' + porta)
+        print e
+
--- Program/sqlite-Funcions.py
+++ Program/sqlite-Funcions.py
@@ -3,6 +3,7 @@ import socket
 import sqlite3
 from threading import *
 
+
 def createDB():
     connSQL = sqlite3.connect('Database/user.db')
     cursor = connSQL.cursor()
@@ -12,20 +13,39 @@ def createDB():
         "CREATE TABLE Users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,ip TEXT,porta TEXT);")
     connSQL.close()
 
-createDB()
 
-def checkOnlineOne(ip,porta):
-    try:
-        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # qw12IPv4,tipo de socket
-        conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        conn.connect((ip, porta))  # Abre uma conexão com IP e porta especificados
-        conn.close()
-    except Exception as e:
-        print('Usuario Offline' + ip + ':' + porta)
-        print e
+def getUsuariosDB():                                #retorna uma matriz/lista no formato usuario=getUsuariosDB()
+    print "getUsuariosDB: started"                  #pode chamar utilizando usuario[i]['ip'] ou usuario[i]['porta']
+    usuarios = []
+    connSQL = sqlite3.connect('Database/user.db')
+    cursor = connSQL.cursor()
+    cursor.execute("SELECT * FROM Users;")
+    data = cursor.fetchall()
+    i = 1
+    if data:
+        for linha in data:
+            Valor={}
+            Valor['ip']= str(linha[1])
+            Valor['porta'] = str(linha[2])
+            print i
+            usuarios.append (Valor)
+            #usuarios[i] = [str(linha[1]), str(linha[2])]
 
 
+            i = i + 1
+    connSQL.close()
+    return usuarios
 
+def addUsuarioDB(ip,porta):                         #addUsuarioDB("123.456.779.231","5050")
+    print "addUsuarioDB: started"
+    connSQL = sqlite3.connect('Database/user.db')
+    cursor = connSQL.cursor()
+    insert_stmt=(
+        "INSERT INTO Users(ip,porta) VALUES ('"+ip+"','"+porta+"')")
+    cursor.execute(insert_stmt)
+    connSQL.commit()
+    connSQL.close()
+    print "addUsuarioDB: finish"
 
 def checkOnlineAll():
     connSQL = sqlite3.connect('Database/user.db')
@@ -33,9 +53,13 @@ def checkOnlineAll():
     cursor.execute("SELECT * FROM Users;")
     data = cursor.fetchall()
     if data:
-    for linha in data:
-        checkOnlineOne(str(linha[1]), str(linha[2]))
+        for linha in data:
+            checkOnlineOne(str(linha[1]), str(linha[2]))
     connSQL.close()
 
-ip= 
-checkOnlineOne(127.0.0.1,porta)
+
+if __name__ == "__main__":
+
+    addUsuarioDB("123.456.779.231","5050")
+    usuarios =  getUsuariosDB()
+    print usuarios
