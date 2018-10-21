3c1c31e23b2905ee3bf004af05551dcc03a2c566
Leandroschwab <schwab_@hotmail.com>
2018-10-21 03:20:28 -0300

    Informações para janelas de chat
    O programa sabe quais janelas estao abertas e qual o destinatario de cada janela. a funçao send envia a mensagem para o destinatario porem este ainda n le.

--- Program/Server.py
+++ Program/Server.py
@@ -3,9 +3,10 @@ import socket
 from ThreadServer import *
 from threading import Thread, Lock, BoundedSemaphore, Semaphore
 
-def server(porta):
+def server(usuarios,):
     HOST = ''                 # Symbolic name meaning all available interfaces
-    PORT = porta               # Arbitrary non-privileged port
+    PORT = input("digite a porta: ")
+                  # Arbitrary non-privileged port
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4,tipo de socket
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT)) #liga o socket com IP e porta
@@ -17,6 +18,6 @@ def server(porta):
         conn, addr = s.accept()  # Aceita uma conexão
         print "Aceitou uma nova conexao"
         print 'Connected by', addr
-        t = Thread(target=novaConn, args=(conn,))
+        t = Thread(target=novaConn, args=(conn,usuarios))
         t.start()
 
