4868cbcccd159832252f08d6931635ebcde3ca33
Leandroschwab <schwab_@hotmail.com>
2018-10-26 20:57:53 -0300

    Leitura da Chave pub e prv
    o programa restaura as chaves publicas e privada

--- Program/Crypto_Functions.py
+++ Program/Crypto_Functions.py
@@ -1,22 +1,43 @@
 import os
 
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
 
 
 def createMyKeys(VarData):
-    try:
-        os.mkdir('Data/' + str(VarData['porta']))
-    except Exception as e:
-        print "pasta do usuario ja existe"
+    if os.path.exists('Data/' + str(VarData['porta']) + '/private.pem'):
+        print 'Carregando chaves!'
+        prv_file = open('Data/' + str(VarData['porta']) + "/private.pem", "r")
+        pub_file = open('Data/' + str(VarData['porta']) + "/public.pem", "r")
+        private_key = RSA.importKey(prv_file.read())
+        public_key = RSA.importKey(pub_file.read())
+        VarData['myprivatekey'] = private_key
+        VarData['mypublickey'] = public_key
 
-    private_key = RSA.generate(1024)
-    public_key = private_key.publickey()
-    print(private_key.exportKey(format='PEM'))
-    print(public_key.exportKey(format='PEM'))
+    else:
+        print 'Criando chave privada!'
+        private_key = RSA.generate(1024)
+        public_key = private_key.publickey()
+        # print(private_key.exportKey(format='PEM'))
+        # print(public_key.exportKey(format='PEM'))
 
-    prv_file = open('Data/' + str(VarData['porta']) + "/private.pem", "w")
-    prv_file.write("{}".format(private_key.exportKey()))
+        VarData['myprivatekey'] = private_key
+        VarData['mypublickey'] = public_key
 
-    pub_file = open('Data/' + str(VarData['porta']) + "/public.pem", "w")
-    pub_file.write("{}".format(public_key.exportKey()))
+        prv_file = open('Data/' + str(VarData['porta']) + "/private.pem", "w")
+        prv_file.write("{}".format(private_key.exportKey()))
+        pub_file = open('Data/' + str(VarData['porta']) + "/public.pem", "w")
+        pub_file.write("{}".format(public_key.exportKey()))
 
+
+if __name__ == "__main__":
+    ''' print "ok"
+ 
+     key = 'mysecretpassword'
+     plaintext = 'secret Message A'
+ 
+     encobj = AES.new(key, AES.MODE_ECB)
+     ciphertext = encobj.encrypt(plaintext)
+ 
+     # Resulting ciphertext in hex
+     print ciphertext.encode('hex')'''
